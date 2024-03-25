67129954a928af96ec703a846bb0422652429b59
Angelo Balchaki <164347105+AngeloBalchaki@users.noreply.github.com>
2024-03-25 10:55:51 -0300

    Add files via upload

---
+++ Exercicio3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+    public static void executar() {
+        public static void calcularSalarioFinal(String nome, double salarioFixo, double totalVendas) {
+            double comissao = totalVendas * 0.15;
+            double salarioFinal = salarioFixo + comissao;
+            System.out.println("Nome do vendedor: " + nome);
+            System.out.println("Salário fixo: R$ " + salarioFixo);
+            System.out.println("Salário final do mês: R$ " + salarioFinal);
+        }
+    }

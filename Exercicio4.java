3490ecda0441edfc2a9ba8391229c8fb96a60daf
Angelo Balchaki <164347105+AngeloBalchaki@users.noreply.github.com>
2024-03-25 10:56:00 -0300

    Add files via upload

---
+++ Exercicio4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Exercicio4 {
+
+    public static void executar() {
+        public static double converterDolarParaReal(double valorDolar, double cotacaoDolar) {
+            return valorDolar * cotacaoDolar;
+        }
+    }
+  
+    

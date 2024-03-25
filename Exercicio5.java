a551269d97944b08f0a6df2a423b68c9320febc1
Angelo Balchaki <164347105+AngeloBalchaki@users.noreply.github.com>
2024-03-25 10:56:14 -0300

    Add files via upload

---
+++ Exercicio5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Exercicio5 {
+
+    public static void executar() {
+        public static void calcularPrestacoes(double valorCompra) {
+            double valorPrestacao = valorCompra / 5;
+            System.out.println("Valor de cada prestação: R$ " + valorPrestacao);
+        }
+    }
